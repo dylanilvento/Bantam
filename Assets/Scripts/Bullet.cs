4b08084ace1d0ae78756214a57af7a6bed39a95b
Dylan Ilvento <dylanilvento@gmail.com>
2022-07-31 18:50:53 -0400

    More Csharpier formatting.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,38 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour
+{
+    public GameObject explosion;
+    int modifier;
 
-	public GameObject explosion;
-	int modifier;
-	// Use this for initialization
-	void Start () {
-		if (transform.localScale.x > 0) {
-			modifier = 1;
-		}
+    // Use this for initialization
+    void Start()
+    {
+        if (transform.localScale.x > 0)
+        {
+            modifier = 1;
+        }
+        else
+        {
+            modifier = -1;
+        }
+    }
 
-		else {
-			modifier = -1;
-		}
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		transform.position = new Vector2(transform.position.x + 0.02f * modifier, transform.position.y);
 	}
 
-	void OnTriggerEnter2D (Collider2D other) {
-		if (other.gameObject.name.Contains("Blade")) {
-			Destroy(gameObject);
-			CreateExplosion();
-		}
-		else if (other.gameObject.name.Contains("Enemy")) {
-			Destroy(gameObject);
-			CreateExplosion();
-		}
-	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name.Contains("Blade"))
+        {
+            Destroy(gameObject);
+            CreateExplosion();
+        }
+        else if (other.gameObject.name.Contains("Enemy"))
+        {
+            Destroy(gameObject);
+            CreateExplosion();
+        }
+    }
 
-	void CreateExplosion () {
-		Instantiate(explosion, transform.position, Quaternion.identity);
-	}
+    void CreateExplosion()
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+    }
 }
--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,193 +3,256 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Character : MonoBehaviour {
-	Rigidbody2D rb;
-	BoxCollider2D boxCollider;
-	CircleCollider2D circleCollider;
+public class Character : MonoBehaviour
+{
+    Rigidbody2D rb;
+    BoxCollider2D boxCollider;
+    CircleCollider2D circleCollider;
 
+    SpriteRenderer sr;
 
-	SpriteRenderer sr;
+    Animator anim;
 
-	Animator anim;
+    GameManager gm;
+    public GameObject bullet,
+        explosion;
+    public float xAxis;
 
-	GameManager gm;
-	public GameObject bullet, explosion;
-	public float xAxis;
+    [Range(0f, 1f)]
+    public float movementSpeed;
 
-	[Range(0f, 1f)]
-	public float movementSpeed;
-	[Range(0f, 50f)]
-	public float jumpDistance;
-	public Image[] uiBullets = new Image[4], reloadBarFill = new Image[5], uiHealth = new Image[4];
-	public Sprite bulletFill, bulletOutline, heartFill, heartOutline;
-	int ammo = 3, health = 3;
-	bool outOfAmmo = false, grounded = true, ducking = false, canShoot = true, invincible = false, dead = false;
+    [Range(0f, 50f)]
+    public float jumpDistance;
+    public Image[] uiBullets = new Image[4],
+        reloadBarFill = new Image[5],
+        uiHealth = new Image[4];
+    public Sprite bulletFill,
+        bulletOutline,
+        heartFill,
+        heartOutline;
+    int ammo = 3,
+        health = 3;
+    bool outOfAmmo = false,
+        grounded = true,
+        ducking = false,
+        canShoot = true,
+        invincible = false,
+        dead = false;
 
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
+        sr = GetComponent<SpriteRenderer>();
+        gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        Transparency.SetTransparent(reloadBarFill);
+    }
 
-	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody2D>();	
-		anim = GetComponent<Animator>();
-		boxCollider = GetComponent<BoxCollider2D>();
-		circleCollider = GetComponent<CircleCollider2D>();
-		sr = GetComponent<SpriteRenderer>();
-		gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
-		Transparency.SetTransparent(reloadBarFill);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-			
-		if (!dead) {
-			xAxis = Input.GetAxis("Horizontal");
+    // Update is called once per frame
+    void Update()
+    {
+        if (!dead)
+        {
+            xAxis = Input.GetAxis("Horizontal");
 
-			if (Input.GetKeyDown(KeyCode.UpArrow) && grounded) {
-				rb.velocity = new Vector2(rb.velocity.x * 2.5f, jumpDistance);
-				grounded = false;
-			}
+            if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x * 2.5f, jumpDistance);
+                grounded = false;
+            }
 
-			if (xAxis > 0f && grounded && !ducking) {
-				rb.velocity = new Vector2 (movementSpeed, rb.velocity.y);
-				if (transform.localScale.x < 0) transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
-				
-			}
+            if (xAxis > 0f && grounded && !ducking)
+            {
+                rb.velocity = new Vector2(movementSpeed, rb.velocity.y);
+                if (transform.localScale.x < 0)
+                    transform.localScale = new Vector2(
+                        transform.localScale.x * -1,
+                        transform.localScale.y
+                    );
+            }
+            else if (xAxis < 0f && grounded && !ducking)
+            {
+                rb.velocity = new Vector2(-1 * movementSpeed, rb.velocity.y);
+                if (transform.localScale.x > 0)
+                    transform.localScale = new Vector2(
+                        transform.localScale.x * -1,
+                        transform.localScale.y
+                    );
+            }
 
-			else if (xAxis < 0f && grounded && !ducking) {
-				rb.velocity = new Vector2 (-1 * movementSpeed, rb.velocity.y);
-				if (transform.localScale.x > 0) transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
-				
-			}
+            if (Input.GetKeyDown(KeyCode.Space) && !outOfAmmo && canShoot)
+            {
+                Shoot();
+            }
 
-			if (Input.GetKeyDown(KeyCode.Space) && !outOfAmmo && canShoot) {
-				Shoot();
-			}
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                anim.SetBool("duck", true);
+                ducking = true;
+                boxCollider.enabled = false;
+            }
 
-			if (Input.GetKeyDown(KeyCode.DownArrow)) {
-				anim.SetBool("duck", true);
-				ducking = true;
-				boxCollider.enabled = false;
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                anim.SetBool("duck", false);
+                ducking = false;
+                boxCollider.enabled = true;
+            }
 
-			}
+            anim.SetFloat("movement", Mathf.Abs(xAxis));
+        }
+    }
 
-			if (Input.GetKeyUp(KeyCode.DownArrow)) {
-				anim.SetBool("duck", false);
-				ducking = false;
-				boxCollider.enabled = true;
-			}
+    void Shoot()
+    {
+        if (!ducking)
+        {
+            GameObject spawnBullet = Instantiate(
+                bullet,
+                new Vector2(
+                    transform.position.x + CheckDirectionForBulletSpawn(),
+                    transform.position.y + 0.03f
+                ),
+                Quaternion.identity
+            );
+            if (transform.localScale.x < 0)
+                spawnBullet.transform.localScale = new Vector2(
+                    spawnBullet.transform.localScale.x * -1,
+                    spawnBullet.transform.localScale.y
+                );
+        }
+        else
+        {
+            GameObject spawnBullet = Instantiate(
+                bullet,
+                new Vector2(
+                    transform.position.x + CheckDirectionForBulletSpawn(),
+                    transform.position.y - 0.1f
+                ),
+                Quaternion.identity
+            );
+            if (transform.localScale.x < 0)
+                spawnBullet.transform.localScale = new Vector2(
+                    spawnBullet.transform.localScale.x * -1,
+                    spawnBullet.transform.localScale.y
+                );
+        }
 
-			anim.SetFloat("movement", Mathf.Abs(xAxis));
-		}
-	}
+        uiBullets[ammo].sprite = bulletOutline;
+        ammo--;
 
-	void Shoot () {
-		if (!ducking) {
-			GameObject spawnBullet = Instantiate(bullet, new Vector2(transform.position.x + CheckDirectionForBulletSpawn(), transform.position.y + 0.03f), Quaternion.identity);
-			if (transform.localScale.x < 0) spawnBullet.transform.localScale = new Vector2 (spawnBullet.transform.localScale.x * -1, spawnBullet.transform.localScale.y);
-		}
+        if (ammo < 0)
+        {
+            outOfAmmo = true;
+            StartCoroutine("Reload");
+        }
 
-		else {
-			GameObject spawnBullet = Instantiate(bullet, new Vector2(transform.position.x + CheckDirectionForBulletSpawn(), transform.position.y - 0.1f), Quaternion.identity);
-			if (transform.localScale.x < 0) spawnBullet.transform.localScale = new Vector2 (spawnBullet.transform.localScale.x * -1, spawnBullet.transform.localScale.y);
-		}
+        StartCoroutine("PauseShooting");
+    }
 
-		uiBullets[ammo].sprite = bulletOutline;
-		ammo--;
+    IEnumerator PauseShooting()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(0.25f);
+        canShoot = true;
+    }
 
-		if (ammo < 0) {
-			outOfAmmo = true;
-			StartCoroutine("Reload");
-		}
+    float CheckDirectionForBulletSpawn()
+    {
+        float diff = 0.09f;
+        if (transform.localScale.x < 0)
+            return diff * -1f;
+        else
+            return diff;
+    }
 
-		StartCoroutine("PauseShooting");
+    IEnumerator Reload()
+    {
+        float reloadRate = 0.25f;
 
-	}
+        foreach (Image fill in reloadBarFill)
+        {
+            yield return new WaitForSeconds(reloadRate);
+            Transparency.SetOpacity(fill, 1f);
+        }
+        yield return new WaitForSeconds(reloadRate);
+        foreach (Image shot in uiBullets)
+        {
+            shot.sprite = bulletFill;
+        }
 
-	IEnumerator PauseShooting () {
-		canShoot = false;
-		yield return new WaitForSeconds(0.25f);
-		canShoot = true;
-	}
+        Transparency.SetTransparent(reloadBarFill);
 
-	float CheckDirectionForBulletSpawn () {
-		float diff = 0.09f;
-		if (transform.localScale.x < 0) return diff * -1f;
-		else return diff;
-	}
+        outOfAmmo = false;
+        ammo = 3;
+    }
 
-	IEnumerator Reload () {
-		float reloadRate = 0.25f;
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (
+            other.gameObject.name.Contains("Floor")
+            || other.gameObject.name.Contains("Enemy Base")
+            || other.gameObject.name.Contains("Platform")
+        )
+        {
+            grounded = true;
+        }
+    }
 
-		foreach (Image fill in reloadBarFill) {
-			yield return new WaitForSeconds(reloadRate);
-			Transparency.SetOpacity(fill, 1f);
-		}
-		yield return new WaitForSeconds(reloadRate);
-		foreach (Image shot in uiBullets) {
-			shot.sprite = bulletFill;
-		}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name.Contains("Blade") && !invincible)
+        {
+            print("Works");
 
-		Transparency.SetTransparent(reloadBarFill);
+            if (health >= 0)
+            {
+                uiHealth[health].GetComponent<Image>().sprite = heartOutline;
+                health--;
+            }
 
-		outOfAmmo = false;
-		ammo = 3;
-		
-	}
+            if (health < 0)
+            {
+                Die();
+            }
+            else
+            {
+                TakeDamage();
+            }
+        }
+    }
 
-	void OnCollisionEnter2D (Collision2D other) {
-		if (other.gameObject.name.Contains("Floor") || other.gameObject.name.Contains("Enemy Base") || other.gameObject.name.Contains("Platform")) {
-			grounded = true;
-		}	
-	}
+    void TakeDamage()
+    {
+        rb.velocity = new Vector2(transform.localScale.x * -1f, 4f);
+        StartCoroutine("FlashInvincibility");
+    }
 
-	void OnTriggerEnter2D (Collider2D other) {
-		if (other.gameObject.name.Contains("Blade") && !invincible) {
-			print("Works");
-			
-			if (health >= 0) {
-				uiHealth[health].GetComponent<Image>().sprite = heartOutline;
-				health--;
-			
-			}
+    void Die()
+    {
+        circleCollider.enabled = false;
+        boxCollider.enabled = false;
+        rb.velocity = new Vector2(transform.localScale.x * -1f, 4f);
+        dead = true;
+        // StartCoroutine("FlashInvincibility");
+        gm.CallGameOver();
+    }
 
-			if (health < 0) {
-				Die();
-
-			}
-
-			else {
-				TakeDamage();
-			}
-		}
-	}
-
-	void TakeDamage () {
-		rb.velocity = new Vector2(transform.localScale.x * -1f, 4f);
-		StartCoroutine("FlashInvincibility");
-	
-	}
-
-	void Die () {
-		circleCollider.enabled = false;
-		boxCollider.enabled = false;
-		rb.velocity = new Vector2(transform.localScale.x * -1f, 4f);
-		dead = true;
-		// StartCoroutine("FlashInvincibility");
-		gm.CallGameOver();
-	
-	}
-
-	IEnumerator FlashInvincibility () {
-		int ii = 0;
-		invincible = true;
-		while (ii < 5) {
-			sr.color = Color.red;
-			yield return new WaitForSeconds(0.25f);
-			sr.color = Color.white;
-			yield return new WaitForSeconds(0.25f);
-			ii++;
-		}
-		invincible = false;
-	}
-	
+    IEnumerator FlashInvincibility()
+    {
+        int ii = 0;
+        invincible = true;
+        while (ii < 5)
+        {
+            sr.color = Color.red;
+            yield return new WaitForSeconds(0.25f);
+            sr.color = Color.white;
+            yield return new WaitForSeconds(0.25f);
+            ii++;
+        }
+        invincible = false;
+    }
 }
